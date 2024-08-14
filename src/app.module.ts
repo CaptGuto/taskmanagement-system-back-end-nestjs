2c41bb6221e7ceb8c647266dc41a6787d65cf449
CaptGuto <tadenafi@gmail.com>
2024-08-14 13:19:14 +0300

    user and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from "./app.service";
 
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeOrmConfig } from "src/config/typeorm.config";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
