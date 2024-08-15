bd5490c4fa13f0901c03c4e378c54d9da7e63d29
CaptGuto <tadenafi@gmail.com>
2024-08-15 14:52:34 +0300

    user entity

---
+++ src/user/persistence/user/user.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Timestamp,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @CreateDateColumn({
+    type: "timestamptz",
+    name: "created_at",
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: "timestamptz",
+    name: "updated_at",
+  })
+  updated_at: Date;
+}
