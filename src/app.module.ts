419b06afb6548ce143996e48414937028ef57cf8
CaptGuto <tadenafi@gmail.com>
2024-08-16 14:46:01 +0300

    d

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { typeOrmConfig } from "src/config/typeorm.config";
+import { typeOrmConfig } from "../config/typeorm.config";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 
