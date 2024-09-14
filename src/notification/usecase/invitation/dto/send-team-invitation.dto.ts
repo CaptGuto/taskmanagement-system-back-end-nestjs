60eecf3f17bd0ddb2cca8c8d4db137738d7c25c5
CaptGuto <tadenafi@gmail.com>
2024-09-14 19:30:59 +0300

    minor addition

--- src/notification/usecase/invitation/dto/send-team-invitation.dto.ts
+++ src/notification/usecase/invitation/dto/send-team-invitation.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsEnum, IsNotEmpty, IsString } from "class-validator";
 import { InvitationType } from "src/notification/common/enums/invitation-type.enum";
 
 export class SendTeamInvitationDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsEnum(InvitationType)
   invitationType: InvitationType;
 
+  @ApiProperty()
   @IsString()
   invitationMessage: string;
 
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   invitedUserEmail: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   inivitationTeamId: number;
 }
